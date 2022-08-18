2bd044d4c352dc1455112611319f74e8c98ce6d3
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-18 18:15:20 +0000

    Completed the functionality

--- main.js
+++ main.js
@@ -1,47 +1,9 @@
-// let round = 0
-// let playerWin = 0
-// let computerWins = 0
-
-// const rock = document.querySelector("#rock").addEventListener('click', chooseRock)
-// const paper = document.querySelector("#paper").addEventListener('click', choosePaper)
-// const scissors = document.querySelector("#scissors").addEventListener('click', chooseScissors)
-// const div = document.querySelector('div')
-
-// function chooseRock() {
-//     playerChoice = 'rock'
-//     console.log(playRound(playerChoice,getComputerChoice()))
-//     if(round === 5){
-//         console.log(playerWin)
-//         console.log(computerWins)
-//         div.innerHTML = `Game over ${playerWin} vs ${computerWins}`
-//     }else {
-//         console.log(computerWins)
-//         console.log(playerWin)
-//         div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
-//     round += 1
-//     }
-// }
-// function choosePaper() {
-//     playerChoice = 'paper'
-//     console.log(playRound(playerChoice,getComputerChoice()))
-//     if(round === 5){
-//         div.innerHTML = `Game over ${playerWin} vs ${computerWins}`
-//     }else {
-//         div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
-//     round += 1
-//     }
-// }
-// function chooseScissors() {
-//     playerChoice = 'scissors'
-//     console.log(playRound(playerChoice,getComputerChoice()))
-//     if(round === 5){
-//         div.innerHTML = `Game over ${playerWin} vs ${computerWins}`
-//     }else {
-//         div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
-//     round += 1
-//     }
-// }
-
+let round = 0
+let playerWon = 0
+let computerWon = 0
+let playerChoice
+const playerScore = document.querySelector('#player')
+const computerScore = document.querySelector('#computer')
 
 function getComputerChoice() {
     const random = Math.random()
@@ -71,10 +33,7 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-let round = 0
-let playerWon = 0
-let computerWon = 0
-let playerChoice
+
 
 const div = document.querySelector('div')
 const rock = document.querySelector('#rock')
@@ -83,40 +42,40 @@ const paper = document.querySelector("#paper")
 
 
 
-function start() {
+rock.addEventListener('click', rockStart)
+paper.addEventListener('click', paperStart)
+scissors.addEventListener('click', scissorsStart)
 
-    rock.addEventListener('click', rockStart)
-    paper.addEventListener('click', paperStart)
-    scissors.addEventListener('click', scissorsStart)
-
-    function rockStart() {
-        playerChoice = 'rock'
-        if(round === 5){
+function rockStart() {
+    playerChoice = 'rock'
+    if(round >= 5){
         div.innerHTML = `Game over ${playerWon} vs ${computerWon}`
     }else {
         div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
-    round += 1
+        playerScore.innerText = `playerScore: ${playerWon}`
+        computerScore.innerText = `computerScore: ${computerWon}`
+        round += 1
     }   
-    }
-
-    function paperStart() {
-        playerChoice = 'paper'
-        if(round === 5){
-            div.innerHTML = `Game over ${playerWon} vs ${computerWon}`
-        }else {
-            div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
-        round += 1
-        }
-    }
-
-    function scissorsStart() {
-        playerChoice = 'scissors'
-        if(round === 5){
+}
+function paperStart() {
+    playerChoice = 'paper'
+    if(round === 5){
         div.innerHTML = `Game over ${playerWon} vs ${computerWon}`
-        }else {
-            div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
+    }else {
+        div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
+        playerScore.innerText = `playerScore: ${playerWon}`
+        computerScore.innerText = `computerScore: ${computerWon}`
         round += 1
-        }
     }
 }
-start()
+function scissorsStart() {
+    playerChoice = 'scissors'
+    if(round === 5){
+    div.innerHTML = `Game over ${playerWon} vs ${computerWon}`
+    }else {
+        div.innerHTML = `${playRound(playerChoice,getComputerChoice())}`
+        playerScore.innerText = `playerScore: ${playerWon}`
+        computerScore.innerText = `computerScore: ${computerWon}`
+        round += 1
+    }
+}
